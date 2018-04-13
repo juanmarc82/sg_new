82721ea5769a9a7fe7bb23ad40074ca1c8ee8b86
Unknown <juanmarojascardenas@gmail.com>
2018-04-13 18:52:09 +0200

    initial commit

---
+++ startApp.php
@@ -0,0 +1,14 @@
+ <?php
+ //Conn param defined
+define("HOST_BBDD","localhost");
+define("USER_BBDD","root");
+define("PASS_BBDD","");
+define("NAME_BBDD","stopgo");
+
+$conexion = mysqli_connect(HOST_BBDD, USER_BBDD, PASS_BBDD, NAME_BBDD);
+// Checking conn.
+if($conexion === false){
+    die("ERROR: Error de conexión " . mysqli_connect_error());
+}
+ 
+?>
